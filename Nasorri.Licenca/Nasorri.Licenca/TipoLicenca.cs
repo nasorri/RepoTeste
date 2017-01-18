c42677cf14319954673755bcaf35958750261b29
Rafael Nasorri <rafaelnasorri@yahoo.com.br>
2017-01-18 09:47:57 -0200

    init

---
+++ Nasorri.Licenca/Nasorri.Licenca/TipoLicenca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasorri.Licenca
+{
+    public class TipoLicenca
+    {
+        public string TRIAL { get; set; }
+    }
+}
